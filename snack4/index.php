31fe83037182c05541f6c48e39985224705be440
Ga <gabri.magni.97@gmail.com>
2022-05-06 11:03:23 +0200

    snack4 while

--- snack4/index.php
+++ snack4/index.php
@@ -19,6 +19,17 @@ $totElInFinalArray = 15;
 // }
 // var_dump($array);
 
+//USA IL WHILE!!!!
+$arrayWhile = [];
+while (count($arrayWhile) < 15) {
+    $numero = rand(1, 15);
+    if (in_array($numero, $arrayWhile) == true) {
+        $arrayWhile[] = $numero;
+    };
+};
+
+var_dump($arrayWhile);
+
 // 2) 2o tentativo
 
 for ($i = 0; $i < $rangeRandNum; $i++) {
