50f53889c31cce32f1f9a461f7a8e4c433c6419e
Ga <gabri.magni.97@gmail.com>
2022-05-07 15:27:19 +0200

    snack7 foreach

--- snack7/index.php
+++ snack7/index.php
@@ -38,12 +38,56 @@ $alunni = [
 var_dump($alunni);
 
 
-for ($i = 0; $i < count($alunni); $i++) {
-    $somma = 0;
-    for ($v = 0; $v < count($alunni[$i]['voti']); $v++) {
-        $somma += $alunni[$i]['voti'][$v]; 
-    };
-    $media = round( ($somma / count($alunni[$i]['voti'])), 2 );
-    echo $alunni[$i]['nome'] . " " . $alunni[$i]['cognome'] . ', media: ' . $media . "<br>";
-};
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <div>
+            <!-- <?php 
+            for ($i = 0; $i < count($alunni); $i++) {
+                $somma = 0;
+                for ($v = 0; $v < count($alunni[$i]['voti']); $v++) {
+                    $somma += $alunni[$i]['voti'][$v]; 
+                };
+                $media = round( ($somma / count($alunni[$i]['voti'])), 2 );
+                echo $alunni[$i]['nome'] . " " . $alunni[$i]['cognome'] . ', media: ' . $media . "<br>";
+            };
+            ?> -->
+
+            <?php 
+            foreach ($alunni as $alunno) { 
+            ?>
+
+                <p>
+
+                <?php 
+                foreach ($alunno as $key => $item) { 
+                    if($key == 'nome') {
+                        echo $item . ", ";
+                    }
+                    if($key == 'cognome') {
+                        echo $item . ", ";
+                    }
+                    if($key == 'voti') {
+                        $sum = array_sum($item);
+                        $media = round( ($sum / count($item)), 2 );
+                        echo "media: " . $media;
+                    } 
+                };
+                ?>
+
+                </p>
+
+            <?php
+            };
+            ?>
+        </div>
+    </body>
+</html>
